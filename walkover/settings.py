e354156f7e5711612adef846951e8edb59dd4bad
Hanish112 <hanish.k.b.112@gmail.com>
2022-06-14 11:24:13 +0530

    added environment fix

--- walkover/settings.py
+++ walkover/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY',default='INSECURE_KEY')
 
 AUTH0_CLIENT_ID = env('AUTH0_CLIENT_ID')
 AUTH0_CLIENT_SECRET = env('AUTH0_CLIENT_SECRET')
