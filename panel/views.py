0b3a4fb124817c3ed88b3e98c4ec86752d7fb3c1
Hanish112 <hanish.k.b.112@gmail.com>
2022-06-14 12:23:44 +0530

    authO-2

--- panel/views.py
+++ panel/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from authlib.integrations.django_client import OAuth
+from django.conf import settings
+
 
 oauth = OAuth()
 
@@ -12,3 +14,14 @@ oauth.register(
     },
     server_metadata_url=f"https://{settings.AUTH0_DOMAIN}/.well-known/openid-configuration",
 )
+
+def dashboard(request):
+    if request.session.get('user') == None:
+        return redirect('login')
+    from django.apps import apps
+    app_models = [model.__name__ for model in apps.get_models(include_auto_created=True)]
+    print(app_models)
+    from django.contrib.contenttypes.models import ContentType
+    ContentType.objects.filter(app_label="auth")
+    session = request.session.get("user")
+    return render(request, 'panel/index.html', {'name': session['userinfo']['name']})
