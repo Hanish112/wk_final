01e2396b38217c7da65f81ecd02b8d3e1e622950
PaviG26 <pavig026@gmail.com>
2022-06-14 12:01:50 +0530

    authO

--- panel/views.py
+++ panel/views.py
@@ -1,1 +1,14 @@
 from django.shortcuts import render, redirect
+from authlib.integrations.django_client import OAuth
+
+oauth = OAuth()
+
+oauth.register(
+    "auth0",
+    client_id=settings.AUTH0_CLIENT_ID,
+    client_secret=settings.AUTH0_CLIENT_SECRET,
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f"https://{settings.AUTH0_DOMAIN}/.well-known/openid-configuration",
+)
