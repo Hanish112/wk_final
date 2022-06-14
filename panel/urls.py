6def444e6c966d2df4c6f12c3fedded8bce5dd25
Hanish112 <hanish.k.b.112@gmail.com>
2022-06-14 15:45:07 +0530

    logout_callback-2

--- panel/urls.py
+++ panel/urls.py
@@ -3,5 +3,7 @@ from panel.views import *
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
     path('create_table/', create_table, name='create_table'),
+    path('callback/', callback, name='callback')
 ]
