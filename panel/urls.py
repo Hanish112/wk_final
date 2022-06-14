dd9a80c118dd9cdb5f95b7e6109cd9874e47a71a
Hanish112 <hanish.k.b.112@gmail.com>
2022-06-14 15:43:17 +0530

    login-2

--- panel/urls.py
+++ panel/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from panel.views import *
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    path('login/', login, name='login'),
     path('create_table/', create_table, name='create_table'),
 ]
